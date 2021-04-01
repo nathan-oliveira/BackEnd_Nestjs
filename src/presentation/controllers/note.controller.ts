174467adb1ec302796fb35bdaccef238d7cf5239
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2021-04-01 08:20:09 -0300

    feat: cadastro de lista

--- src/presentation/controllers/note.controller.ts
+++ src/presentation/controllers/note.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Inject, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { Request, Response } from "express";
 import { NoteService } from "../../domain/services/note.service"
 import { RolesGuard } from "../../domain/roles/roles.guard"
+import { HttpResult } from "src/common/helpers/http-result";
 
 //"title": "string",
 //"content": "string",
@@ -14,7 +15,17 @@ export class NoteController {
   @Post("/notes")
   async create(@Req() req: Request, @Res() res: Response): Promise<Response> {
     const { userId } = (req as unknown) as { userId: number };
+    const dataForm = { ...req.body, userId };
 
-    return res.status(200).json({ idUser: userId })
+    try {
+      const result = await this.noteService.createNote(dataForm);
+      const payload = HttpResult.ok(result)
+
+      return res.status(payload.statusCode).json(payload.body);
+    } catch (err) {
+      const payload = HttpResult.badRequest(err);
+
+      return res.status(payload.statusCode).json(payload.body);
+    }
   }
 }

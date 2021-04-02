2d360bc1db3010f09154413861fde3caeddac7e3
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2021-04-01 22:09:21 -0300

    chore: ajuste config jest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,26 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
-// import { AppModule } from './../src/app.module';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppController } from './../src/presentation/controllers/app.controller'
+import { AppService } from './../src/domain/services/app.service'
 
-// describe('AppController (e2e)', () => {
-//   let app: INestApplication;
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
 
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).compile();
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [],
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
 
-//     app = moduleFixture.createNestApplication();
-//     await app.init();
-//   });
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
 
-//   it('/ (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-// });
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+  });
+});

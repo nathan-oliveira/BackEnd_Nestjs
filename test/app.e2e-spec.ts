47b2d75286e763534c00d21d2d21d2fe6d0b975a
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2021-04-02 10:08:50 -0300

    refactor: jest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,20 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/domain/repo/app.module';
-import server from "src/data/infrastructure/config/server"
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    await server.createServer();
-    app = server.app;
-    // const moduleFixture: TestingModule = await Test.createTestingModule({
-    //   imports: [AppModule],
-    // }).compile();
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-    // app = moduleFixture.createNestApplication();
-    // await app.init();
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
   afterEach(async () => {
